3ccc98b2f885b410ac1aa9ad1ffe68550c209c61
Cholmes303 <cj@holmesholdingsllc.com>
2025-02-25 13:39:20 -0700

    server change

--- client.py
+++ client.py
@@ -3,11 +3,8 @@ import socket
 # Create a socket object
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-host = '127.0.0.1'
-port = 2000
-
 # Connect to server
-client.connect((host, port))
+client.connect(('127.0.0.1', 2000))
 print("Connected to server")
 
 # Send messages and receive responses
