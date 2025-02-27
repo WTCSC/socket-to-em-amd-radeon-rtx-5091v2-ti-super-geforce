082865b4e6e05b05316ea0204c0d205441cd0bae
cristianh <cj@jasonholmes.net>
2025-02-27 12:17:47 -0700

    Commented out client / server code

--- client2.py
+++ client2.py
@@ -3,52 +3,67 @@ from threading import Thread
 import tkinter
 from tkinter import *
 
+"""Function used to receive messages."""
 def receive():
     while True:
         try:
             message = client.recv(1024).decode()
             message_list.insert(tkinter.END, message)
         except Exception:
-            print("An error occured while receiving the message")
+            print("An error occured while receiving the message.")
 
+"""Function used to send messages."""
 def send():
     message = my_message.get()
     my_message.set("")
     client.send(bytes(message, "utf8"))
 
+# Creates the tkinter application.
 window = Tk()
+# Names the application.
 window.title("Chat room")
+# Color scheme.
 window.configure(bg="white")
 
+# This creates the box where the messages will exist and update.
 message_frame = Frame(window, height=100, width=100, bg="white")
 message_frame.pack()
 
+# Creates a string variable for the user's message.
 my_message = StringVar()
 my_message.set("")
 
+# Adds a scroll bar to view older messages in chat room. 
 scroll_bar = Scrollbar(message_frame)
+# Merges the frame and scroll bar together. 
 message_list = Listbox(message_frame, height=15, width=100, bg="white", yscrollcommand=scroll_bar.set)
+# Puts the scroll bar on the right side of the frame.
 scroll_bar.pack(side=RIGHT, fill=Y)
 message_list.pack(side=LEFT, fill=BOTH)
 message_list.pack()
 
+# Adds a box where the user can type messages that will be sent. 
 entry_field = Entry(window, textvariable=my_message, fg="black", width=50)
 entry_field.pack()
 
+# Creates a button that will be used to send messages into the chat room. 
 send_button = Button(window, text="Send", font="Aerial", fg="white", bg="black", command=send)
 send_button.pack()
 
+# IP address and port used to connect clients to the server. 
 host = "localhost"
 port = 2000
 
-# Create a socket object
+# Create a socket object.
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Connect to server
+# Connect to server. 
 client.connect((host, port))
 print("Connected to server")
 
+# Thread used to allow multiple client requests to the server at the same time. 
 receive_thread = Thread(target=receive)
 receive_thread.start()
 
+"""This function is used to keep the tkinter function running until the application window is closed."""
 mainloop()
--- server.py
+++ server.py
@@ -13,35 +13,47 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind((host, port))
 
+"""Function used to handle clients that connect to the server."""
 def handle_clients(connection):
+    # Recovers an 8 bit message (the name of the client) and decodes it. 
     name = connection.recv(1024).decode()
     welcome_message = f"Welcome {name} to our chat room!"
+    # Server sends a direct message to the client that has recently joined.
     connection.send(bytes(welcome_message, "utf8"))
     message = name + " has recently joined the chat room."
+    # Used to display a message from the server to all clients. 
+    # This broadcast message is who has recently joined the chat room. 
     broadcast(bytes(message, "utf8"))
 
+    # Saves the clients name into the dictionary.
     clients[connection] = name
 
     while True:
         message = connection.recv(1024)
         broadcast(message, name + ":")
 
+"""Function used to broadcast messages to all clients connected to the server."""
 def broadcast(message, prefix=""):
     for client in clients: 
         client.send(bytes(prefix, "utf8") + message)
 
+"""Function used to accept clients requesting to join the chat room."""
 def accept_client_connection():
     while True:
+        # Used to accept clients 
         client_connection, client_address = server.accept()
         print(client_address, " has connected")
 
-        client_connection.send(bytes("Welcome to the chat room. Enter your name to continue", "utf8"))
+        client_connection.send(bytes("Welcome to the chat room. Enter your name to continue", "utf8")) 
+        # Starts multi-threading.
         Thread(target=handle_clients, args=(client_connection,)).start()
 
 if __name__ == "__main__":
+    # Allows up to 3 client connections to the server.
     server.listen(3)
     print("listening on port : ", port, "...")
 
+    # Allows for the server to accept multiple requests at once. 
     requests = Thread(target=accept_client_connection)
 
     requests.start()
